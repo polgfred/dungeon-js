309cec8aa850571c4cfa22bf7af8c6667a8cccc2
Frederick Polgardy <fred@polgardy.com>
2026-01-25 19:34:31 +0000

    get rid of pigment, too unstable to set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { createTheme } from "@mui/material";
-import { pigment } from "@pigment-css/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    pigment({
-      transformLibraries: ["@mui/material"],
-      theme: createTheme({ cssVariables: true }),
-    }),
-    react(),
-  ],
+  plugins: [react()],
   server: {
     host: true,
   },
