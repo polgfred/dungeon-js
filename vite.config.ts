be406e13482965f5512d77b1ce8fb6f4f645114a
Frederick Polgardy <fred@polgardy.com>
2026-01-27 20:06:11 +0000

    add vite build stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import compression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Enable if your host serves precompressed assets with Content-Encoding.
+    // compression(),
+    // compression({ algorithm: "brotliCompress", ext: ".br" }),
+  ],
   server: {
     host: true,
+  },
+  build: {
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
   },
 });
